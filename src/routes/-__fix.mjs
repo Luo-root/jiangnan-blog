bcb19a05f47d7d71952bd8dfccf6cb48b3d97307
luo-root <3029295957@qq.com>
2026-08-17 12:27:56 +0800

    fix: 从 git 移除 build/dev 日志和 src/routes/-__fix.* debug 残留

--- src/routes/-__fix.mjs
+++
@@ -1,6 +0,0 @@
-import { readFileSync, writeFileSync } from "fs";
-const p = 'D:/Code/Front-end/博客/src/routes/_layout.posts_.$slug.tsx';
-let s = readFileSync(p, "utf8");
-s = s.replace(/\nconsole\.log.*$/m, "");
-writeFileSync(p, s);
-console.log("cleaned, len =", s.length);
